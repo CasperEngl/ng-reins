b6606e4455147bff7d36c0fbe3c398e4833e6eec
Casper Engelmann <me@casperengelmann.com>
2018-11-27 23:47:45 +0100

    refactor of products$ to products

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,19 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
 
 import { WordpressService } from '../wordpress.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent implements OnInit {
-  products$: any[];
+  products: any[];
 
   constructor(private wp: WordpressService) { }
 
   ngOnInit() {
-    this.wp.products.subscribe(response => this.products$ = response);
+    this.wp.products.subscribe(response => this.products = response);
   }
 }
