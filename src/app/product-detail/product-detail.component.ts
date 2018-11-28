7e815efbd059f5560ab396c64d93dcd6a626ae7d
Casper Engelmann <me@casperengelmann.com>
2018-11-28 14:08:24 +0100

    refactor of params id

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -20,10 +20,8 @@ export class ProductDetailComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.route.params.subscribe(params => {
-      this.id = Number(params['id']);
-
       this.productSubscription = this.wp.products.subscribe(
-        response => this.product$ = response.find(product => product.id === this.id)
+        response => this.product$ = response.find(product => product.id === Number(params['id']))
       );
     });
   }
