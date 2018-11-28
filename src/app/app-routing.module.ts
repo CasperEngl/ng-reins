7dfce15de56297211c37842b66d66136fc6a81a0
Casper Engelmann <me@casperengelmann.com>
2018-11-28 23:19:05 +0100

    Added DelayResolve to delay routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,23 +5,28 @@ import { ProductDetailComponent } from './product-detail/product-detail.componen
 import { ProductsComponent } from './products/products.component';
 import { HomeComponent } from './home/home.component';
 import { CartComponent } from './cart/cart.component';
+import { DelayResolve } from './transition.resolver';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+    resolve: [DelayResolve],
   },
   {
     path: 'products',
     component: ProductsComponent,
+    resolve: [DelayResolve],
   },
   {
     path: 'product/:id',
     component: ProductDetailComponent,
+    resolve: [DelayResolve],
   },
   {
     path: 'cart',
     component: CartComponent,
+    resolve: [DelayResolve],
   },
 ];
 
@@ -29,6 +34,7 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes, {
     // enableTracing: true
   })],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [DelayResolve],
 })
 export class AppRoutingModule { }
